cc98f756c72cd0c835475f3cb24b63f5c7aa03db
viraj992 <it21183836@my.sliit.lk>
2025-08-04 18:52:14 +0530

    Initial Frontend Setup commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <ProductCard
+        name = "Samsung Galaxy"/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard(props){
+
+
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <img src = "https://picsum.photos/seed/picsum/200/300"/>
+        </div>
+    )
+}
