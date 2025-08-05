0ec98e628f6a8fb3eb7e2815643df6575dbe5162
viraj992 <it21183836@my.sliit.lk>
2025-08-05 17:29:45 +0530

    flex, justify-content , items-center

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import ProductCard from './components/productCard'
+import "./App.css";
 
 function App() {
-  
-
-  return (
-    <>
-      <div>
-        <ProductCard
-        name = "Samsung Galaxy"/>
-      </div>
-    </>
-  )
+	return (
+		<div className="w-full h-screen bg-blue-300">
+			<div className="w-[600px] h-[600px] bg-black flex flex-col justify-center ">
+				<div className=" w-[75px] h-[75px] bg-yellow-600"></div>
+				<div className="w-[75px] h-[75px] bg-red-900"></div>
+				<div className="w-[75px] h-[75px] bg-green-600"></div>
+				<div className="w-[75px] h-[75px] bg-purple-600"></div>
+				<div className="w-[75px] h-[75px] bg-pink-600"></div>
+				<div className="w-[75px] h-[75px] bg-orange-600"></div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
