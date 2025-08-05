967173fb5900a95fc992ae6ad911fb67dbb49fc4
viraj992 <it21183836@my.sliit.lk>
2025-08-05 17:43:05 +0530

    fixed vs absolute

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import "./App.css";
 function App() {
 	return (
 		<div className="w-full h-screen bg-blue-300">
-			<div className="w-[600px] h-[600px] bg-black flex flex-col justify-center ">
+			<div className="w-[600px] h-[600px] bg-black flex flex-col justify-center relative ">
 				<div className=" w-[75px] h-[75px] bg-yellow-600"></div>
 				<div className="w-[75px] h-[75px] bg-red-900"></div>
 				<div className="w-[75px] h-[75px] bg-green-600"></div>
-				<div className="w-[75px] h-[75px] bg-purple-600"></div>
-				<div className="w-[75px] h-[75px] bg-pink-600"></div>
-				<div className="w-[75px] h-[75px] bg-orange-600"></div>
+				<div className="w-[75px] h-[75px] bg-purple-600 fixed top-0 right-0"></div>
+				<div className="w-[75px] h-[75px] bg-pink-600 absolute top-0 right-0"></div>
+				<div className="w-[75px] h-[75px] bg-orange-600 "></div>
 			</div>
 		</div>
 	);
