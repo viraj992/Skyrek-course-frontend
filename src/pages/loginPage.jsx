337c0e8b9232f0f6a7201294f5787ddf11f783a9
viraj992 <it21183836@my.sliit.lk>
2025-08-09 19:44:44 +0530

    LoginPage Button onlick function 🔐

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,21 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function login(){
+        console.log(email, password)
+        axios.post("http://localhost:5000/api/users/login",{
+            email: email,
+            password : password
+        }).then(
+            (response)=>{
+                console.log(response.data)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }
+    
+
     return(
         <div className="w-full h-screen bg-[url(./loginbg.jpg)] bg-cover bg-center flex justify-center items-center">
-            <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] relative text-blue-500 flex flex-col items-center justify-center">
+            <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] relative gap-[20px] text-blue-500 flex flex-col items-center justify-center">
                 <h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
 
                 <div className="w-[350px] flex flex-col ">
                     <span className="text-lg ">Email</span>
-                    <input type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                    <input onChange={
+                        (e)=>{
+                            setEmail(e.target.value)
+                        }
+                    } type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
                 </div>
                 <div className="w-[350px] flex flex-col ">
                     <span className="text-lg ">Password</span>
-                    <input type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+
+                    <input onChange={
+                        (e)=>{
+                            setPassword(e.target.value);
+                        }
+                    } type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
                 </div>
 
-                <button className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition-all duration-300">
+                <button onClick={login} className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition-all duration-300">
                     Login
                 </button>
 
