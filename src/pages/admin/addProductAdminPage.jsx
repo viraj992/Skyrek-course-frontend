45919819f4606dfbad50c744654609c0fbaec50a
viraj992 <it21183836@my.sliit.lk>
2025-08-11 15:11:47 +0530

    matching backend model's inputs ✅  solved

--- src/pages/admin/addProductAdminPage.jsx
+++ src/pages/admin/addProductAdminPage.jsx
@@ -1,132 +1,184 @@
+import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function AddProductPage(){
-    const [productId, setProductId] = useState("");
-    const [productName, setProductName] = useState("");
-    const [alternativeNames, setAlternativeNames] = useState("");
-    const [labeledPrice, setLabeledPrice] = useState("");
-    const [price, setPrice] = useState("");
-    const [images, setImages] = useState([]);
+export default function AddProductPage() {
+	const [productId, setProductId] = useState("");
+	const [productName, setProductName] = useState("");
+	const [alternativeNames, setAlternativeNames] = useState("");
+	const [labelledPrice, setLabelledPrice] = useState("");
+	const [price, setPrice] = useState("");
+	const [images, setImages] = useState("");
 	const [description, setDescription] = useState("");
 	const [stock, setStock] = useState("");
 	const [isAvailable, setIsAvailable] = useState(true);
 	const [category, setCategory] = useState("cream");
-    
+    const navigate = useNavigate()
 
-    return(
-        <div className="w-full h-full flex justify-center items-center">
-            <div className="w-[600px] border-[3px] rounded-[15px] p-[40px] flex flex-wrap justify-between">
+    function handleSubmit(){
+        const altNamesInArray = alternativeNames.split(",")
+        const productData = {
+            productId: productId,
+            name: productName,
+            altNames: altNamesInArray,
+            labelledPrice: labelledPrice,
+            price: price,
+            images: [],
+            description: description,
+            stock: stock,
+            isAvailable: isAvailable,
+            category: category
+        }
 
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Product ID</label>
-                    <input type="text"
-                    value={productId}
-                    onChange={(e) => {
-                        setProductId(e.target.value)
-                    }}
-                    className="w-full border-[1px] h-[40px] rounded-md"
-                    />
-                </div>
+        const token = localStorage.getItem("token");
 
-                <div className="w-[300px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Product Name</label>
-                    <input
+        if(token == null){
+            window.location.href = "/login";
+            return;
+        }
+
+        axios.post(import.meta.env.VITE_BACKEND_URL + "/api/products", productData, 
+            {
+                headers:{
+                    Authorization: "Bearer "+token
+                }
+            }
+        ).then(
+            (res)=>{
+                console.log("Product added successfully");
+                console.log(res.data);
+                toast.success("Product added successfully");
+                navigate("/admin/products");
+            }
+        ).catch(
+            (error)=>{
+                console.error("Error adding product:", error);
+                toast.error("Failed to add product");              
+            }
+        )
+
+        console.log(productData);
+
+
+    }
+
+	return (
+		<div className="w-full h-full flex justify-center items-center">
+			<div className="w-[600px] border-[3px] rounded-[15px] p-[40px] flex flex-wrap justify-between">
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Product ID</label>
+					<input
+						type="text"
+						value={productId}
+						onChange={(e) => {
+							setProductId(e.target.value);
+						}}
+						className="w-full border-[1px] h-[40px] rounded-md"
+					/>
+				</div>
+				<div className="w-[300px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Product Name</label>
+					<input
 						type="text"
 						value={productName}
 						onChange={(e) => setProductName(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[500px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Alternative Names</label>
-                    <input
+				</div>
+				<div className="w-[500px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Alternative Names</label>
+					<input
 						type="text"
 						value={alternativeNames}
 						onChange={(e) => setAlternativeNames(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Labeled Price</label>
-                    <input
+				</div>
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Labelled Price</label>
+					<input
 						type="number"
-						value={labeledPrice}
-						onChange={(e) => setLabeledPrice(e.target.value)}
+						value={labelledPrice}
+						onChange={(e) => setLabelledPrice(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Price</label>
-                    <input
+				</div>
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Price</label>
+					<input
 						type="number"
 						value={price}
 						onChange={(e) => setPrice(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[500px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Images</label>
-                    <input
-						multiple
-						type="file"
-						onChange={(e) => {
-							setImages(e.target.files);
-						}}
+				</div>
+				<div className="w-[500px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Images</label>
+					<input
+						type="text"
+						value={images}
+						onChange={(e) => setImages(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[500px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Description</label>
-                    <textarea
+				</div>
+				<div className="w-[500px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Description</label>
+					<textarea
 						value={description}
 						onChange={(e) => setDescription(e.target.value)}
 						className="w-full border-[1px] h-[100px] rounded-md"
 					></textarea>
-                </div>
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Stock</label>
-                    <input
+				</div>
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Stock</label>
+					<input
 						type="number"
 						value={stock}
 						onChange={(e) => setStock(e.target.value)}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					/>
-                </div>
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">is Available</label>
-                    <select
+				</div>
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Is Available</label>
+					<select
 						value={isAvailable}
 						onChange={(e) => {
 							setIsAvailable(e.target.value === "true");
 						}}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					>
-                        <option value={true}>Available</option>
-                        <option value={false}>Not Available</option>
-                    </select>
-                </div>
-                <div className="w-[200px] flex flex-col gap-[5px]">
-                    <label className="tex-sm font-semibold">Category</label>
-                   <select
+						<option value={true}>Available</option>
+						<option value={false}>Not Available</option>
+					</select>
+				</div>
+				<div className="w-[200px] flex flex-col gap-[5px]">
+					<label className="text-sm font-semibold">Category</label>
+					<select
 						value={category}
 						onChange={(e) => {
 							setCategory(e.target.value);
 						}}
 						className="w-full border-[1px] h-[40px] rounded-md"
 					>
-                        <option value={"cream"}>Cream</option>
-                        <option value={"face wash"}>Face Wash</option>
-                        <option value={"soap"}>Soap</option>
-                        <option value={"fragrance"}>Fragrance</option>
-                   </select>
-                </div>
-                <div className="w-full flex justify-center flex-row py-[20px]">
-                        <Link to={"/admin/products"} className="w-[200px] h-[50px] bg-white text-black border-[2px] rounded-md flex justify-center items-center">Cancel</Link>
-                        <button className="w-[200px] h-[50px] bg-black text-white border-[2px] rounded-md flex justify-center items-center ml-[20px]">Add Product</button>
-                </div>
-
-            </div>
-        </div>
-    )
+						<option value="cream">Cream</option>
+						<option value="face wash">Face Wash</option>
+						<option value="soap">Soap</option>
+						<option value="fragrance">Fragrance</option>
+					</select>
+				</div>
+				<div className="w-full flex justify-center flex-row py-[20px]">
+					<Link
+						to={"/admin/products"}
+						className="w-[200px] h-[50px] bg-white text-black border-[2px] rounded-md flex justify-center items-center"
+					>
+						Cancel
+					</Link>
+					<button onClick={handleSubmit} className="w-[200px] h-[50px] bg-black text-white border-[2px] rounded-md flex justify-center items-center ml-[20px]">
+						Add Product
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
