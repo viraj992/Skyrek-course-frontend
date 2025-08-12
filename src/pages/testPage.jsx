6f585a3835cddfa6a154945fb4196a640b9742b2
viraj992 <it21183836@my.sliit.lk>
2025-08-12 11:24:58 +0530

    tested image upload supabase succesfully ✅

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,13 +1,37 @@
-//const url "https://gahctdfvkjlwulpijbxo.supabase.co"
-//const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaGN0ZGZ2a2psd3VscGlqYnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NzEzMzMsImV4cCI6MjA3MDU0NzMzM30.Hrmt4EkR9-Pdj6CbZAuDANavu5nueYNiTPvwBPyjYrw"
-import { useState } from "react"
+const url = "https://gahctdfvkjlwulpijbxo.supabase.co";
 
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaGN0ZGZ2a2psd3VscGlqYnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NzEzMzMsImV4cCI6MjA3MDU0NzMzM30.Hrmt4EkR9-Pdj6CbZAuDANavu5nueYNiTPvwBPyjYrw"
+
+import { useState } from "react"
+import { createClient } from "@supabase/supabase-js";
+import toast from "react-hot-toast";
+
+const supabase = createClient(url,key)
 
 export default function TestPage(){
     const [file, setFile] = useState(null);
     
     function handleUpload(){
         console.log(file)
+
+        if(file == null){
+            reject("Please select a file to upload");
+            return;
+        }
+
+        supabase.storage.from("images").upload(file.name,file,{
+            cacheControl: "3600",
+            upsert: false
+        }).then(
+            ()=>{
+                toast.success("file uploaded successfully");
+                }
+        ).catch(
+            (error)=>{
+                    console.error("Error uploading file", error);
+                    toast.error("failed to upload file")
+                }
+            )
     }
     return(
         <div className="w-full h-screen flex justify-center items-center">
