465e405e703f43e47c695132cc9a0d7c96ff615f
viraj992 <it21183836@my.sliit.lk>
2025-08-12 14:21:18 +0530

    file upload function created ✅✏️

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,37 +1,28 @@
-const url = "https://gahctdfvkjlwulpijbxo.supabase.co";
-
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaGN0ZGZ2a2psd3VscGlqYnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NzEzMzMsImV4cCI6MjA3MDU0NzMzM30.Hrmt4EkR9-Pdj6CbZAuDANavu5nueYNiTPvwBPyjYrw"
 
 import { useState } from "react"
-import { createClient } from "@supabase/supabase-js";
-import toast from "react-hot-toast";
 
-const supabase = createClient(url,key)
+import toast from "react-hot-toast";
+import uploadFile from "../utils/mediaUpload";
+
+
 
 export default function TestPage(){
     const [file, setFile] = useState(null);
     
     function handleUpload(){
-        console.log(file)
-
-        if(file == null){
-            reject("Please select a file to upload");
-            return;
-        }
-
-        supabase.storage.from("images").upload(file.name,file,{
-            cacheControl: "3600",
-            upsert: false
-        }).then(
-            ()=>{
-                toast.success("file uploaded successfully");
-                }
+        uploadFile(file).then(
+            (url)=>{
+                console.log(url)
+                toast.success("File uploaded succesfully")
+            }
         ).catch(
             (error)=>{
-                    console.error("Error uploading file", error);
-                    toast.error("failed to upload file")
-                }
-            )
+                console.error("Error uploading file:",error);
+                toast.error("error")
+            }
+        )
+
+        
     }
     return(
         <div className="w-full h-screen flex justify-center items-center">
--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,3 +1,39 @@
+const url = "https://gahctdfvkjlwulpijbxo.supabase.co";
+
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaGN0ZGZ2a2psd3VscGlqYnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NzEzMzMsImV4cCI6MjA3MDU0NzMzM30.Hrmt4EkR9-Pdj6CbZAuDANavu5nueYNiTPvwBPyjYrw"
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(url,key)
+
 export default function uploadFile(file){
-    
+    const promise = new Promise(
+        (resolve, reject)=>{
+            
+            if( file == null){
+                reject("Please select a file to upload");
+                return;
+            }
+
+            const timeStamp = new Date().getTime();  // for unique name 
+            const fileName = timeStamp+ "-"+file.name
+
+            supabase.storage.from("images").upload(fileName,file,{
+            cacheControl: "3600",
+            upsert: false
+        }).then(
+            ()=>{
+                const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+            
+                resolve(publicUrl)
+                }
+        ).catch(
+            (error)=>{
+                    
+                    reject("failed to upload file");
+                }
+            )
+        }
+    )
+    return promise;
 }
