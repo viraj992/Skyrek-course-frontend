11d4d61fc40e0724fff3472977157cc588fb269a
viraj992 <it21183836@my.sliit.lk>
2025-08-22 19:19:01 +0530

    Product Card Component Implemented ✏️

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,10 +1,28 @@
 export default function ProductCard(props){
+    const product = props.product
 
 
     return(
-        <div>
-            <h1>{props.name}</h1>
-            <img src = "https://picsum.photos/seed/picsum/200/300"/>
+        <div className="w-[300px] h-[400px] flex flex-col shrink-0 shadow-2xl rounded-2xl overflow-hidden">
+            <img src={product.images[0]} className="w-full h-[275px] object-cover"/>
+            <div className="w-full h-[125px] flex flex-col">
+                <span className="text-gray-400 text-[12px]">{product.productId}</span>
+                <h1 className="text-xl font-bold">
+                    {product.name} {" "}
+                    <span className="text-gray-500 text-[14px]">{product.category}</span>
+                </h1>
+                <div>
+                    {  product.labelledPrice > product.price ? (
+                        <p>
+                            <span className="line-through mr-[10px]">{product.labelledPrice.toFixed(2)}</span>
+                            <span>{product.price.toFixed(2)}</span>
+                        </p>
+                    ) : (
+                        <span> {product.price.toFixed(2)} </span>
+
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/client/productsPage.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import Loader from "../../components/loader";
+import axios from "axios";
+import ProductCard from "../../components/productCard";
+
+export default function ProductPage(){
+    const [prodcuts,setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(
+        ()=>{
+            if (loading){
+                axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products").then(
+                (res)=>{
+                    setProducts(res.data);
+                    setLoading(false);
+                    });
+                }
+            }, [loading]
+    );
+
+    return(
+        <div className="w-full h-full">
+            {
+                loading ? <Loader/> : 
+                <div className="w-full flex flex-wrap gap-[40px] justify-center items-center p-[20px]">
+                    {
+                        prodcuts.map((product)=>{
+                            return(
+                                <ProductCard key={product.productId} product={product} />
+                                    
+                                
+                            )
+                        })
+                    }
+                </div>
+            }
+        </div>
+    )
+}
