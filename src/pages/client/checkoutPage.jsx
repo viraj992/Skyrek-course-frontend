07ea218a9fd8557c6e5929734ea9ca158fea9046
viraj992 <it21183836@my.sliit.lk>
2025-09-09 13:33:57 +0530

    checkout Page needed things ✏️

--- src/pages/client/checkoutPage.jsx
+++ src/pages/client/checkoutPage.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react"
-import { TbTrash } from "react-icons/tb"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useState } from "react";
 
-export default function CheckOutpage(){
-    const location = useLocation();
-    const navigate = useNavigate;
-    const [cart, setCart] = useState(location.state.items || []);
-    if(location.state.items == null){
-        toast.console.error("Please select items to checkout");
-        navigate("/products");
-    }
+import { TbTrash } from "react-icons/tb";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-    function getTotal() {
+export default function CheckoutPage() {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [cart, setCart] = useState(location.state.items || []);
+	if (location.state.items == null) {
+		toast.error("Please select items to checkout");
+		navigate("/products");
+	}
+
+	function getTotal() {
 		let total = 0;
 		cart.forEach((item) => {
 			total += item.quantity * item.price;
@@ -19,57 +22,128 @@ export default function CheckOutpage(){
 		return total;
 	}
 
-    
-    return(
-        <div className="w-full h-screen flex flex-col items-center py-[40px]">
-            {
-                cart.map(
-                    (item)=>{
-                        return(
-                            <div key={item.productId} className="w-[800px] h-[100px] m-[10px] shadow-2xl flex flex-row items-center relative">
-                                <img src={item.image} className="w-[100px] h-[100px] object-cover"/>
+	async function placeOrder() {
+		const token = localStorage.getItem("token");
+		if (token == null) {
+			toast.error("Please login to place an order");
+			navigate("/login");
+			return;
+		}
+		const order = {
+			address: "df",
+			phone: "df",
+            items : []
+		};
+        cart.forEach((item) => {
+            order.items.push({
+                productId: item.productId,
+                qty: item.quantity
+            })
+        })
 
-                                <div className="w-[320px] h-full flex flex-col justify-center pl-[10px]">
-                                    <span className=" font-bold">{item.name}</span>
-                                    <span className="font-semibold">{item.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                                </div>
+        try{
+            await axios.post(import.meta.env.VITE_BACKEND_URL + "/api/orders", order, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            toast.success("Order placed successfully");
+            
 
-                                <div className="w-[190px] h-full flex flex-row justify-center items-center ">
-                                    <button className="flex justify-center items-center w-[30px] rounded-lg bg-blue-600 text-white  cursor-pointer hover:bg-blue-400" onClick={
-                                        ()=>{
-                                            
-                                        }
-                                    }>-</button>
-                                    <span className="mx-[10px]">{item.quantity}</span>
-                                    <button className="flex justify-center items-center w-[30px] rounded-lg bg-blue-600 text-white cursor-pointer hover:bg-blue-400" onClick={
-                                        ()=>{
-                                            
-                                        }
-                                    } >+</button>
-                                </div>
-                                <div className="w-[190px] h-full flex justify-end items-center pr-[10px]">
-                                    <span className="font-semibold">{(item.quantity * item.price).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                                </div>
-                                <button className="w-[30px] h-[30px] absolute right-[-40px] cursor-pointer bg-red-700 shadow rounded-full flex justify-center items-center text-white border-red-700 border-[2px] hover:bg-white hover:text-red-700" onClick={
-                                    ()=>{
-                                        
-                                    }
-                                }>
-                                    <TbTrash className="text-xl"/>
-                                </button>
-                            </div>
-                        )
-                    }
-                )
-            }
-            <div className="w-[800px] h-[100px] m-[10px] p-[10px] shadow-2xl flex flex-row items-center justify-end relative">
-                <span className="font-bold text-2xl">
-                    Total: {getTotal().toLocaleString("en-us", { minimumFractionDigits:2, maximumFractionDigits:2})}
-                </span>
-                <button className="absolute left-[10px] w-[150px] h-[50px] cursor-pointer rounded-lg shadow-2xl bg-blue-700 border-[2px] border-blue-700 text-white hover:bg-white hover:text-blue-700">
-                    Place Order
-                </button>
-            </div>
-        </div>
-    )
+        }catch(err){
+            console.error(err);
+            toast.error("Failed to place order");
+            return;
+        }
+	}
+
+	console.log(cart);
+	return (
+		<div className="w-full h-screen flex flex-col py-[40px] items-center">
+			{cart.map((item, index) => {
+				return (
+					<div
+						key={item.productId}
+						className="w-[800px] h-[100px] m-[10px] shadow-2xl flex flex-row items-center relative"
+					>
+						<img
+							src={item.image}
+							className="w-[100px] h-[100px] object-cover"
+						/>
+						<div className="w-[320px] h-full  flex flex-col justify-center pl-[10px]">
+							<span className=" font-bold">{item.name}</span>
+							{/* price */}
+							<span className=" font-semibold">
+								{item.price.toLocaleString("en-US", {
+									minimumFractionDigits: 2,
+									maximumFractionDigits: 2,
+								})}
+							</span>
+						</div>
+						<div className="w-[190px] h-full  flex flex-row justify-center items-center">
+							<button
+								className="flex justify-center items-center w-[30px] rounded-lg bg-blue-600 text-white cursor-pointer hover:bg-blue-400"
+								onClick={() => {
+									const newCart = [...cart];
+									newCart[index].quantity -= 1;
+									if (newCart[index].quantity <= 0) {
+										newCart.splice(index, 1);
+									}
+									setCart(newCart);
+								}}
+							>
+								-
+							</button>
+							<span className="mx-[10px]">{item.quantity}</span>
+							<button
+								className="flex justify-center items-center w-[30px] rounded-lg bg-blue-600 text-white cursor-pointer hover:bg-blue-400"
+								onClick={() => {
+									const newCart = [...cart];
+									//json copy
+									// const student = { name : "John", age: 20 };
+									// const studentCopy = {...student};
+
+									newCart[index].quantity += 1;
+									setCart(newCart);
+								}}
+							>
+								+
+							</button>
+						</div>
+						<div className="w-[190px] h-full flex justify-end items-center pr-[10px]">
+							{/* total quantity * price */}
+							<span className="font-semibold">
+								{(item.quantity * item.price).toLocaleString("en-US", {
+									minimumFractionDigits: 2,
+									maximumFractionDigits: 2,
+								})}
+							</span>
+						</div>
+						<button
+							className="w-[30px] h-[30px] absolute right-[-40px] cursor-pointer bg-red-700 shadow rounded-full flex justify-center items-center text-white border-[2px] border-red-700 hover:bg-white hover:text-red-700"
+							onClick={() => {
+								const newCart = [...cart];
+								newCart.splice(index, 1);
+								setCart(newCart);
+							}}
+						>
+							<TbTrash className="text-xl" />
+						</button>
+					</div>
+				);
+			})}
+			<div className="w-[800px] h-[100px] m-[10px] p-[10px] shadow-2xl flex flex-row items-center justify-end relative">
+				<span className="font-bold text-2xl ">
+					Total:{" "}
+					{getTotal().toLocaleString("en-US", {
+						minimumFractionDigits: 2,
+						maximumFractionDigits: 2,
+					})}
+				</span>
+				<button onClick={placeOrder} className="absolute left-[10px] w-[150px] h-[50px] cursor-pointer rounded-lg shadow-2xl bg-blue-700 border-[2px] border-blue-700 text-white hover:bg-white hover:text-blue-700">
+					Place Order
+				</button>
+			</div>
+		</div>
+	);
 }
--- src/pages/client/productOverView.jsx
+++ src/pages/client/productOverView.jsx
@@ -1,83 +1,89 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-import toast from "react-hot-toast";
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../components/loader";
 import ImageSlider from "../../components/imageSlider";
 import { addToCart, getCart } from "../../utils/cart";
+import toast from "react-hot-toast";
 
 export default function ProductOverViewPage(){
     const params = useParams()
-    const[product,setProduct] = useState(null);
-    const[status, setStatus] = useState("loading"); //loading, sucess, error
-
+    const [product, setProduct] = useState(null);
+    const navigate = useNavigate();
+    const [status, setStatus] = useState("loading"); //loading, success, error
     useEffect(
         ()=>{
             if(status === "loading"){
                 axios.get(import.meta.env.VITE_BACKEND_URL + `/api/products/${params.productId}`).then(
                     (res)=>{
-                        //console.log(res.data);
                         setProduct(res.data);
                         setStatus("success");
                     }
                 ).catch(
-                    (error)=>{
-                        //toast.error("Failed to fetch product details");
-                        setStatus("error");
+                    ()=>{
+                        setStatus("error");                        
                     }
                 )
             }
         }, [status]
     )
-
-    return(
-        <div className="w-full h-full"> 
-           {
-             status == "loading" && <Loader/>
-           }
-
-           {
-             status == "success" && <div className="w-full h-full flex flex-row">
-                <div className="w-[49%] h-full flex flex-col justify-center items-center">
-                    <ImageSlider images={product.images}/>
-                </div>
-
-                {/* Right Side  */}
-                <div className="w-[49%] h-full flex flex-col items-center pt-[50px]">
-                   <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span></h1>
-                   <p className="text-lg mt-[20px]">{product.description}</p>
-                   <div className="w-full flex flex-col items-center mt-[20px]">
-                      {
-                        product.labelledPrice > product.price? 
-                        <div>
-                            <span className="text-2xl font-semibold line-through mr-[20px]">{product.labelledPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                            <span className="text-3xl font-bold ">{product.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                        </div>
-                        :
-                        <div>
-                            <span className="text-3xl font-bold">{product.price}</span>
-                        </div>
-                      }
-                   </div>
-                   <div className="w-full flex flex-row justify-center items-center mt-[20px] gap-[10px]">
-                      <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-900 border-[3px] border-blue-900 hover:bg-white hover:text-blue-900">Buy Now</button>
-                      <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-600 border-[3px] border-blue-600 hover:bg-white hover:text-blue-500" 
-                         onClick={
-                            ()=>{
-                                addToCart(product,1)
-                                toast.success("Product added to cart")
-                                console.log(getCart())
+    
+    return (
+        <div className="w-full h-full">
+            {
+                status == "loading" && <Loader/>
+            }
+            {
+                status == "success" && <div className="w-full h-full flex flex-row">
+                    <div className="w-[49%] h-full flex flex-col justify-center items-center">
+                        <ImageSlider images={product.images} />
+                    </div>
+                    <div className="w-[49%] h-full flex flex-col items-center pt-[50px]">
+                        <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span></h1>
+                        <p className="text-lg mt-[20px]">{product.description}</p>
+                        <div className="w-full flex flex-col items-center mt-[20px]">
+                            {
+                                product.labelledPrice > product.price?
+                                <div>
+                                    <span className="text-2xl font-semibold  line-through mr-[20px]">{product.labelledPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> 
+                                    <span className="text-3xl font-bold ">{product.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                                </div>
+                                :
+                                <div>
+                                    <span className="text-3xl font-bold ">{product.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                                </div>
                             }
-                         }>Add to Cart</button>
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center mt-[20px]  gap-[10px]">
+                            <button
+                            onClick={
+                                ()=>{
+                                    navigate("/checkout", { state: { items: 
+                                        [{
+                                            productId: product.productId,
+                                            quantity: 1,
+                                            name: product.name,
+                                            image: product.images[0],
+                                            price: product.price
+                                        }]
+                                     } });
+                                }
+                            }
+                             className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-900 border-[3px] border-blue-900 hover:bg-white hover:text-blue-900">Buy Now</button>
+                            <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-600 border-[3px] border-blue-600 hover:bg-white hover:text-blue-600" onClick={
+                                ()=>{
+                                    addToCart(product,1)
+                                    toast.success("Product added to cart")
+                                    console.log(getCart())
+                                }
+                            } >Add to Cart</button>
+                        </div>
                     </div>
                 </div>
-             </div>
-           }
-
-
-           {
-             status == "error" && <div>Error Loading Product</div>
-           }
+            }
+            {
+                status == "error" && <div>Error loading product</div>
+            }
         </div>
-    )
+    );
 }
