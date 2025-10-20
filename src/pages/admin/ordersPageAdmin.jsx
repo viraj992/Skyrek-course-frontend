f816da35cd93e502e27a6ba2a623e71d278714cf
viraj992 <it21183836@my.sliit.lk>
2025-10-20 19:51:44 +0530

    Orders Popup window added ✏️📦

--- src/pages/admin/ordersPageAdmin.jsx
+++ src/pages/admin/ordersPageAdmin.jsx
@@ -4,10 +4,14 @@ import Paginator from "../../components/paginator";
 
 export default function OrdersPageAdmin(){
     const [orders, setOrders] = useState([]);
+
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
     const [limit, setLimit] = useState(10);
+
+    const [popupVisible,setPopupVisible] = useState(false);
+    const [clickedOrder, setClickedOrder] = useState(null);
 
     useEffect(() => {
         if(loading){
@@ -29,8 +33,8 @@ export default function OrdersPageAdmin(){
     },[loading,page,limit]);
         
     return(
-        <div className="w-full h-full flex flex-col items-start"> 
-            <table className="w-full border-[3px] mt-[10px]">
+        <div className="w-full h-full flex flex-col justify-between items-start"> 
+            <table className="w-full border-[3px] mt-[20px]">
                 <thead>
                     <tr>
                         <th className="p-[10px]">Order ID</th>
@@ -47,7 +51,11 @@ export default function OrdersPageAdmin(){
                     {
                         orders.map((order,index)=>{
                             return(
-                            <tr key={order.orderID}>
+                            <tr key={order.orderID} className="border-b-[1px] hover:bg-blue-600 hover:text-white"
+                            onClick={()=>{
+                                setClickedOrder(order);
+                                setPopupVisible(true);
+                            }}>
                                 <td className="p-[10px]">{order.orderID}</td>
                                 <td className="p-[10px]">{order.email}</td>
                                 <td className="p-[10px]">{order.name}</td>
@@ -62,6 +70,23 @@ export default function OrdersPageAdmin(){
                     }
                 </tbody>
             </table>
+            {
+                popupVisible && (
+                    <div className="fixed top-0 left-0 w-full h-full bg-[#00000050] flex justify-center items-center">
+                        <div className="w-[600px] h-[600px] top-[12px] bg-white relative">
+                            {
+                                clickedOrder.orderID 
+                            }
+                            <button className="absolute w-[30px] h-[30px] bg-red-600 border-[2px] border-red-600 text-white top-[-28px] right-[-28px] rounded-full cursor-pointer hover:bg-transparent hover:text-red-600"
+                            onClick={()=>{
+                                setPopupVisible(false);
+                            }}>
+                                X
+                            </button>
+                        </div>
+                    </div>
+                )
+            }
             <Paginator currentPage={page} totalPages={totalPages} setCurrentPage={setPage}
 				limit={limit} setLimit={setLimit} setLoading={setLoading}/>
         </div>
