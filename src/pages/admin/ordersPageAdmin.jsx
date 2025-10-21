a335069e5d0fa47f54d8a871039bb8c65af8dce2
viraj992 <it21183836@my.sliit.lk>
2025-10-21 14:41:38 +0530

    toast imported ✏️

--- src/pages/admin/ordersPageAdmin.jsx
+++ src/pages/admin/ordersPageAdmin.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import Paginator from "../../components/paginator";
+import toast from "react-hot-toast";
 
 export default function OrdersPageAdmin(){
     const [orders, setOrders] = useState([]);
